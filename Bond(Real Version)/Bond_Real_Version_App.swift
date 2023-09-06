cd07a39ceabd5f4cba21e56fafcc26880bbca12d
EeshPara <eeshpara@gmail.com>
2023-09-06 12:52:22 -0500

    Initial Commit

---
+++ Bond(Real Version)/Bond_Real_Version_App.swift
@@ -0,0 +1,17 @@
+//
+//  Bond_Real_Version_App.swift
+//  Bond(Real Version)
+//
+//  Created by Eeshwar Parasuramuni on 9/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Bond_Real_Version_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
